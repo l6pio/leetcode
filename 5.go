3ced8d9b594c54bc398995df6e60c80cbf4e0ecb
l6pio <l6padmin@163.com>
2021-09-24 09:07:12 +0800

    commit

---
+++ 5.go
@@ -0,0 +1,32 @@
+package main
+
+func longestPalindrome(s string) string {
+	var m [][]bool
+	sLen := len(s)
+
+	m = make([][]bool, sLen)
+	for i := 0; i < sLen; i++ {
+		m[i] = make([]bool, sLen)
+	}
+
+	ret := ""
+	maxLen := 0
+	for i := 0; i < sLen; i++ {
+		for j := 0; j <= i; j++ {
+			candidateLen := 0
+			if s[j] == s[i] && (j == i || j == i-1 || m[j+1][i-1]) {
+				m[j][i] = true
+				candidateLen = i - j + 1
+			}
+			if candidateLen > maxLen {
+				maxLen = candidateLen
+				ret = s[j : i+1]
+			}
+		}
+	}
+	return ret
+}
+
+func main() {
+	longestPalindrome("")
+}
