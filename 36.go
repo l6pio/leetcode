34955b8fe47e6444bca3a5dc5659c81adfafa712
l6pio <l6padmin@163.com>
2021-10-01 22:03:43 +0800

    commit

---
+++ 36.go
@@ -0,0 +1,32 @@
+package main
+
+import "strconv"
+
+// https://leetcode-cn.com/problems/count-and-say/
+
+func countAndSay(n int) string {
+	ret := "1"
+	for i := 1; i < n; i++ {
+		tmp := ""
+		count := 1
+		left := 0
+		for left < len(ret) - 1 {
+			if ret[left] == ret[left + 1] {
+				count += 1
+				left += 1
+				continue
+			}
+			tmp += strconv.Itoa(count) + string(ret[left])
+			count = 1
+			left += 1
+		}
+		tmp += strconv.Itoa(count) + string(ret[left])
+		ret = tmp
+	}
+	return ret
+}
+
+//func main()  {
+//	say := countAndSay(10)
+//	println(say)
+//}
